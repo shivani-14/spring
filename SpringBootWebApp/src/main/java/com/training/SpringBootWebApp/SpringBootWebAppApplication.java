fd7aab06e9477170c5af3953e73666d964452f22
shivani-14 <68033966+shivani-14@users.noreply.github.com>
2020-11-13 21:37:08 +0800

    SpringBootApplication and SpringBootWebApp

---
+++ SpringBootWebApp/src/main/java/com/training/SpringBootWebApp/SpringBootWebAppApplication.java
@@ -0,0 +1,14 @@
+package com.training.SpringBootWebApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@SpringBootApplication
+public class SpringBootWebAppApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWebAppApplication.class, args);
+	}
+
+}
